4e6b5109ab431918a07cb0f826d327633a5c5076
Brian Wong <bwong92@gmail.com>
2019-07-30 06:37:27 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -15,6 +15,6 @@ def map_to_square(source_array)
   source_array.map { |num| num ** 2}
 end
 
-def reduce_to_total(source_array,starting_point)
+def reduce_to_total(source_array
   source_array.reduce { |num| starting_point + num}
 end
