2caea3975cbf829b477ac1899896dc7be6d5ce34
Brian Wong <bwong92@gmail.com>
2019-07-30 14:42:28 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -21,10 +21,10 @@ end
 
 def reduce_to_all_true(source_array)
   source_array.reduce do |ele|
-    if ele == true
-      return true
-    else
+    if ele != true
       return false
+    else
+      return true
     end
   end
 end
